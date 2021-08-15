30f7f4e6a4a392eb0fad4e3a1f83d2caffa83164
aroxu <30624945+aroxu@users.noreply.github.com>
2021-08-15 18:28:17 +0900

    기본 구성 설정 변경

--- config/configLoader.go
+++ config/configLoader.go
@@ -55,10 +55,9 @@ func generateConfig() {
   "restart": true,
   "memory": 4,
   "plugins": [
-    "https://github.com/monun/kotlin-plugin/releases/latest/download/Kotlin-1.5.10.jar",
-    "https://github.com/dmulloy2/ProtocolLib/releases/latest/download/ProtocolLib.jar"
+    "https://github.com/monun/auto-reloader/releases/latest/download/AutoReloader.jar"
   ],
-  "jarArgs": [""]
+  "jarArgs": ["nogui"]
 }`)
 
 	if errWrtConf != nil {
